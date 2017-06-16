9ef33b7ad1efdb91bb99b70dccded448769daf7c
albuq <barbara.albuquerque@usp.br>
2017-06-15 22:06:34 -0300

    novo LBP + Hog : treinamento, validação, acurácia e matriz confusão

--- subHOG.py
+++ subHOG.py
@@ -1,9 +1,8 @@
 # coding=utf-8
 import cv2
-from skimage.feature import local_binary_pattern
 import numpy
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import confusion_matrix
 from LabelLetra import LabelLetra
 import time
 from datetime import datetime
@@ -14,11 +13,18 @@ import codecs
 import matplotlib.pyplot as plt
 import itertools
 
-
 # configurações do descritor
-n_points = 24
-radius = 3
-method = 'uniform'
+winSize = (128, 128)
+blockSize = (16, 16)
+blockStride = (16, 16)
+cellSize = (16, 16)
+nbins = 9
+derivAperture = 1
+winSigma = -1
+histogramNormType = 0
+L2HysThreshold = 0.2
+gammaCorrection = 1
+nlevels = 64
 
 # configurações da rede
 # MLPClassifier: configurações da rede
@@ -45,13 +51,13 @@ beta_2 = 0.999
 epsilon = 1e-08
 
 # configurações da pasta principal
-pasta_origem = "descritores\\lbp\\"
+pasta_origem = "descritores\\hog\\"
 
 
 def rodaTudo():
-
-    # gera os descritores
     '''
+    # gera os descritores
+
     print("\nComeçando a leitura descritor")
     horario_inicio = datetime.now()
     treino_entrada, teste_entrada = geraDescritor()
@@ -66,7 +72,6 @@ def rodaTudo():
         myfile.write(tempo_descrever_imagens)
     myfile.close()
     '''
-
 
     # le o descritor gerado
     treino_entrada, teste_entrada, treino_saida, teste_saida = leitorDescritor()
@@ -104,7 +109,7 @@ def geraArrayLetras():
 
     labelE = LabelLetra("E", "45")
     letras.append(labelE)
-    
+
     labelF = LabelLetra("F", "46")
     letras.append(labelF)
 
@@ -171,20 +176,13 @@ def geraArrayLetras():
     return letras
 
 
-# metodo para calcular o descritor LBP
+# metodo para calcular o descritor HOG
 def calculaDescritor(imagem):
-    lbp = local_binary_pattern(imagem, n_points, radius, 'uniform')
+    hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins, derivAperture, winSigma,
+                                   histogramNormType, L2HysThreshold, gammaCorrection, nlevels)
 
 
-    (hist, _) = numpy.histogram(lbp.ravel(),
-                             bins=numpy.arange(0, n_points + 3),
-                             range=(0, n_points + 2))
-
-
-    # normaliza o histograma
-    hist = hist.astype("float")
-    hist /= (hist.sum() + 1e-7)
-    return hist
+    return hog.compute(imagem)
 
 
 # metodo para gerar os descritores das imagens
@@ -197,9 +195,9 @@ def geraDescritor():
     for letraLab in letras:
         # gera descritor para as imagens treinamento
         for cont in range(0, 1000):
-            nomeArquivo = cv2.imread("dataset2\\treinamento\\train_" + letraLab.label + "_00" + "{0:03}".format(cont) + ".png")
-            imagem = cv2.cvtColor(nomeArquivo, cv2.COLOR_BGR2GRAY)
-            entrada_treino.append(calculaDescritor(imagem))
+            nomeArquivo = cv2.imread(
+                "dataset2\\treinamento\\train_" + letraLab.label + "_00" + "{0:03}".format(cont) + ".png")
+            entrada_treino.append(calculaDescritor(nomeArquivo))
 
         print("Salvando Treinamento " + letraLab.letra + ", tamanho:" + str(len(entrada_treino)))
         numpy.save(pasta_origem + "Treinamentos_" + letraLab.letra, entrada_treino)
@@ -207,15 +205,14 @@ def geraDescritor():
         del entrada_treino[:]
 
         for cont in range(0, 300):
-            nomeArquivo = cv2.imread("dataset2\\testes\\train_" + letraLab.label + "_01" + "{0:03}".format(cont) + ".png")
-            imagem = cv2.cvtColor(nomeArquivo, cv2.COLOR_BGR2GRAY)
-            entrada_teste.append(calculaDescritor(imagem))
+            nomeArquivo = cv2.imread(
+                "dataset2\\testes\\train_" + letraLab.label + "_01" + "{0:03}".format(cont) + ".png")
+            entrada_teste.append(calculaDescritor(nomeArquivo))
 
         print("Salvando Testes " + letraLab.letra + ", tamanho:" + str(len(entrada_teste)))
         numpy.save(pasta_origem + "Testes_" + letraLab.letra, entrada_teste)
 
         del entrada_teste[:]
-
 
     # retorna as listas de descritores geradas
     return entrada_treino, entrada_teste
@@ -250,14 +247,13 @@ def controlaRede(treino_entrada, teste_entrada, treino_saida, teste_saida):
     configtxt = []
 
     rede = MLPClassifier(hidden_layer_sizes, activation, solver, alpha, batch_size, learning_rate, learning_rate_init,
-                         power_t, max_iter, shuffle, random_state, tol, verbose, warm_start, momentum, nesterovs_momentum,
-                         early_stopping, validation_fraction, beta_1, beta_2, epsilon)
+                         power_t, max_iter, shuffle, random_state, tol, verbose, warm_start, momentum,
+                         nesterovs_momentum, early_stopping, validation_fraction, beta_1, beta_2, epsilon)
 
     print("Tamanho da lista de Treinamento: " + str(len(treino_entrada)))
     print("Tamanho da lista de saida_treino: " + str(len(treino_saida)))
     print("Tamanho da lista de Teste: " + str(len(teste_entrada)))
     print("Tamanho da lista de saida_teste: " + str(len(teste_saida)))
-
 
     # corrige dimensao do array de treinamento
     treino_entrada = numpy.array(treino_entrada)
@@ -268,8 +264,6 @@ def controlaRede(treino_entrada, teste_entrada, treino_saida, teste_saida):
     teste_entrada = numpy.array(teste_entrada)
     teste_entrada = teste_entrada.reshape(len(teste_entrada), -1)
     teste_saida = numpy.array(teste_saida)
-
-
 
     print("Iniciando Treinamento da rede...")
 
@@ -287,7 +281,7 @@ def controlaRede(treino_entrada, teste_entrada, treino_saida, teste_saida):
         resposta_treino, resposta_teste = treino_saida[idTreino], treino_saida[idTeste]
 
         # converte esses datasets para arrays
-        entrada_treino =  numpy.array(entrada_treino)
+        entrada_treino = numpy.array(entrada_treino)
         resposta_treino = numpy.array(resposta_treino)
 
         # treina a rede: gera o erro de treinamento
@@ -299,7 +293,6 @@ def controlaRede(treino_entrada, teste_entrada, treino_saida, teste_saida):
         # atualiza epoca
         epoca = epoca + 1
 
-
     # TESTA A REDE: com a partição de testes
     print("Testando a rede com  a particao de testes...")
 
@@ -307,9 +300,8 @@ def controlaRede(treino_entrada, teste_entrada, treino_saida, teste_saida):
     acuracia = rede.score(teste_entrada, teste_saida)
     print ("Acuracia de: ", acuracia)
 
-    #imprime erro de validação
+    # imprime erro de validação
     print ("Erro de validacao eh: ", erro_validacao)
-
 
     # confusion_matrix Returns the Confusion matrix: entrada(predito, experado)
     matriz_confusao = confusion_matrix(teste_saida, rede.predict(teste_entrada))
@@ -319,32 +311,35 @@ def controlaRede(treino_entrada, teste_entrada, treino_saida, teste_saida):
         classeLetra.append(letraLab.letra)
     print classeLetra
 
-
     plot_confusion_matrix(matriz_confusao, classes=classeLetra, normalize=False, title="Matriz de confusao")
     plot_confusion_matrix(matriz_confusao, classes=classeLetra, normalize=True, title="Matriz de confusao normalizada")
-
 
     # gera o model.dat
     matrizPesos = numpy.asarray(rede.coefs_)
     pickle.dump(matrizPesos, open(pasta_origem + "model.dat", "wb"))
     print ("- salva model.dat")
 
-
     # gera arquivo config.txt
     configtxtdata = ("Execucao em " + time.strftime("%d/%m/%Y") + " " + time.strftime("%H:%M"))
 
     # os parametros do descritor
-    configlbp = ("\n\nLBP (descritor) \nnum_points: %s \nradius: %s \nmethod: %s" % (n_points, radius, method))
+    confighog = ("\n\nHOG (descritor) \nwinSize: %s \nblockSize: %s \nblockStride: %s \ncellSize: %s \nnbins: %s \n"
+                 "derivAperture: %s \nwinSigma: %s \nhistogramNormType: %s \nL2HysThreshold: %s \ngammaCorrection: %s \n"
+                 "nlevels: %s" % (winSize, blockSize, blockStride, cellSize, nbins, derivAperture, winSigma,
+                                  histogramNormType, L2HysThreshold, gammaCorrection, nlevels))
 
     # os parametros da rede
-    configrede = ("\n\nMLPClassifier (rede)\nhidden_layer_sizes : %s\nactivation : %s\nsolver : %s\nalpha : %s\nbatch_size : %s\n"
-        "learning_rate : %s\nlearning_rate_init : %s\npower_t : %s\nmax_iter : %s\nshuffle : %s\n"
-        "random_state : %s\ntol : %s\nverbose : %s\nwarm_start : %s\nmomentum : %s\nnesterovs_momentum : %s\n"
-        "early_stopping : %s\nvalidation_fraction : %s\nbeta_1 : %s\nbeta_2 : %s\nepsilon : %s" %
-        (str(hidden_layer_sizes), str(activation), str(solver), str(alpha), str(batch_size), str(learning_rate),
-         str(learning_rate_init), str(power_t), str(max_iter), str(shuffle), str(random_state), str(tol),
-         str(verbose), str(warm_start), str(momentum), str(nesterovs_momentum), str(early_stopping),
-         str(validation_fraction), str(beta_1), str(beta_2), str(epsilon)))
+    configrede = (
+    "\n\nMLPClassifier (rede)\nhidden_layer_sizes : %s\nactivation : %s\nsolver : %s\nalpha : %s\nbatch_size : %s\n"
+    "learning_rate : %s\nlearning_rate_init : %s\npower_t : %s\nmax_iter : %s\nshuffle : %s\n"
+    "random_state : %s\ntol : %s\nverbose : %s\nwarm_start : %s\nmomentum : %s\nnesterovs_momentum : %s\n"
+    "early_stopping : %s\nvalidation_fraction : %s\nbeta_1 : %s\nbeta_2 : %s\nepsilon : %s" %
+    (str(hidden_layer_sizes), str(activation), str(solver), str(alpha), str(batch_size), str(learning_rate),
+     str(learning_rate_init), str(power_t), str(max_iter), str(shuffle), str(random_state), str(tol),
+     str(verbose), str(warm_start), str(momentum), str(nesterovs_momentum), str(early_stopping),
+     str(validation_fraction), str(beta_1), str(beta_2), str(epsilon)))
+
+
 
     try:
         os.remove(pasta_origem + "config.txt")
@@ -353,14 +348,13 @@ def controlaRede(treino_entrada, teste_entrada, treino_saida, teste_saida):
 
     with codecs.open(pasta_origem + "config.txt", "a", "utf-8") as myfile:
         myfile.write(configtxtdata)
-        myfile.write(configlbp)
+        myfile.write(confighog)
         myfile.write(configrede)
     myfile.close()
     print ("- salva config.txt")
 
     # retorna
     return
-
 
 
 # plota a matriz de confusão
